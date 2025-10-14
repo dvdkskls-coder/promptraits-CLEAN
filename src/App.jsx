558711c2b6a039d8d814a5692c0fe817b7f45473
dvdkskls-coder <dvdkskls@gmail.com>
2025-10-15 01:21:49 +0200

    logo

--- src/App.jsx
+++ src/App.jsx
@@ -837,7 +837,7 @@ return (
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                     <div className="flex justify-between items-center h-20">
 			<button onClick={() => w("home")} className="flex items-center">
-    									<img src="/logo promptraits.svg" alt="Promptraits Logo" className="w-[300px] h-auto" />
+    									<img src="/logo.svg" alt="Promptraits Logo" className="w-[300px] h-auto" />
 			</button>                        
                         <div className="hidden md:flex items-center space-x-8">
                             <button onClick={() => navigateToPage('gallery')} className="text-gray-300 hover:text-white transition duration-300">Galería</button>
